2bb62662c9368fda161f5284ae5c373703fa9ead
Spenbert02 <spenbert02@gmail.com>
2022-01-09 22:11:59 -0600

    idk

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static")
 
 
 @app.route("/")
